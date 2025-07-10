e9e49103d0ee6d7208d3361eb871762e9344ad69
GeraldGichimu <sir.gikonyogichimu1@gmail.com>
2025-07-10 22:32:59 +0300

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,27 +1,7 @@
 <?php
 session_start();
-
-// Unset all session variables
-$_SESSION = array();
-
-// Delete the session cookie
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(), 
-        '', 
-        time() - 42000,
-        $params["path"], 
-        $params["domain"],
-        $params["secure"], 
-        $params["httponly"]
-    );
-}
-
-// Destroy the session
+session_unset();
 session_destroy();
-
-// Redirect to login page
-header("Location: login.php?logout=1");
+header("Location: index.php");
 exit();
 ?>
